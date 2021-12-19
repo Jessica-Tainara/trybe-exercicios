360e0ef0e4698dc318d39b801acc166e40b2ded0
jessica <jessicathaynnara@gmail.com>
2021-12-19 00:07:56 -0300

    exercicios 5.1

--- fundamentos-do-desenvolvimento-web/bloco-4-introduçao-a-JavaScript/4.2-Array-e-loop-for/exercicio-de-fixaçao-loop-for.js
+++
@@ -1,7 +0,0 @@
-// Exercício 1
-// Utilize o for para imprimir os elementos da lista groceryList com o console.log() :
-
-let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
-for (let i = 0; i < groceryList.length; i += 1) {
-    console.log(groceryList[i])
-}
