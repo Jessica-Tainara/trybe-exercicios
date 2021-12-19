ed6f551f4b80aac77a345567b2fd53391bbcc43b
Jessica-Tainara <93151762+Jessica-Tainara@users.noreply.github.com>
2021-12-18 23:54:37 -0300

    Update exercicio-de-fixaçao-variaveis.js

--- fundamentos-do-desenvolvimento-web/bloco-4-introduçao-a-JavaScript/4.1-Primeiros-passos/exercicio-de-fixaçao-variaveis.js
+++ fundamentos-do-desenvolvimento-web/bloco-4-introduçao-a-JavaScript/4.1-Primeiros-passos/exercicio-de-fixaçao-variaveis.js
@@ -1,1 +1,16 @@
-
+// Crie uma constante chamada myName e atribua a ela o seu nome (Exemplo: Carolina).
+const myName = 'Jessica'
+// Crie uma constante chamada birthCity e atribua a ela a sua cidade natal.
+const birthCity = 'Brasilia-DF'
+// Crie uma variável chamada birthYear e atribua a ela o ano em que você nasceu.
+let birthYear = 1996
+// Utilize o console.log() para imprimir as constantes e variáveis que você criou.
+console.log (myName)
+console.log (birthCity)
+console.log (birthYear)
+// Altere o valor atribuído à variável birthYear para 2030. Faça um console.log(birthYear) novamente para ver o que acontece!
+birthYear = 2030
+console.log (birthYear)
+// Altere o valor atribuído à constante birthCity . Faça um console.log(birthCity) novamente! Você saberia explicar por que recebemos uma mensagem de erro? 🤔
+birthCity = 'Gama-DF'
+console.log(birthCity)
